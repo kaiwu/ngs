442b9859081f2205ec0ce57aeb8b6059da462b1a
Kai WU <kaiwu2004@gmail.com>
2024-08-02 12:16:12 +0800

    request ffi

--- src/njs/ngx_ffi.mjs
+++ src/njs/ngx_ffi.mjs
@@ -1,5 +1,37 @@
 import { Ok, Error } from "./gleam.mjs"
 
 export function to_headers(h) {
-  return Headers(h);
+  return new Headers(h);
+}
+
+export function to_request(u, o) {
+  return new Request(u, o);
+}
+
+export function request_has_body(r) {
+  return r.bodyUsed;
+}
+
+export function request_headers(r) {
+  return r.headers;
+}
+
+export function request_method(r) {
+  return r.method;
+}
+
+export function request_url(r) {
+  return r.url;
+}
+
+export function request_body_array_buffer(r) {
+  return r.arrayBuffer()
+}
+
+export function request_body_json(r) {
+  return r.json()
+}
+
+export function request_body_text(r) {
+  return r.text()
 }
